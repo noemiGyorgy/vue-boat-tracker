9092271661f90c31d3e32420af84f4338da73bdc
noemi <gynoemi33@gmail.com>
2021-03-12 07:28:32 +0100

    Create stop recording button

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -14,21 +14,21 @@
         <span>UPLOAD NEW FILE</span>
       </a>
     </div>
+    <StopRecordingButton />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import StopRecordingButton from "./StopRecordingButton.vue";
 
-@Component({})
+@Component({
+  components: { StopRecordingButton }
+})
 export default class Sidebar extends Vue {
   private url_upload: string =
     process.env.VUE_APP_UPLOAD || "http://localhost:5000";
-
-  mounted() {
-    console.log(this.url_upload);
-  }
 }
 </script>
 
---
+++ src/components/StopRecordingButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-button
+      id="stop"
+      type="button"
+      variant="outline-info"
+      class="p-4 pl-5 pr-5"
+      @click="changeStatus"
+    >
+      {{ value }}
+    </b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { tracksStore } from "../store/modules/tracks";
+
+@Component({})
+export default class StopRecordingButton extends Vue {
+  private value = tracksStore.stopped ? "START RECORDING" : "STOP RECORDING";
+
+  changeStatus() {
+    tracksStore.setStopped(!tracksStore.stopped);
+    this.value = tracksStore.stopped ? "START RECORDING" : "STOP RECORDING";
+    console.log(tracksStore.stopped);
+  }
+}
+</script>
+
+<style scoped></style>
